efb7c3801ce075f74f1ef1d7e4f7cf9e85dcc7c9
Vitaliy <vitaliymontana@yandex.ru>
2021-04-22 17:46:55 +0300

    fixed js, buttuns

--- scripts/index.js
+++ scripts/index.js
@@ -4,28 +4,30 @@ const popup = document.querySelector('.popup');
 const closedPopupButton = document.querySelector('.popup__close');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
 
 const profileTitle = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-function togglePopup(event) {
-    event.preventDefault();
-    popup.classList.toggle('popup_opened');
-
+function openPopup() {
+    popup.classList.add('popup_opened');
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileDescription.textContent;
 }
 
-openPopupButton.addEventListener('click', togglePopup);
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
 
-closedPopupButton.addEventListener('click', togglePopup);
+openPopupButton.addEventListener('click', openPopup);
+
+closedPopupButton.addEventListener('click', closePopup);
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    popup.classList.toggle('popup_opened');
+    closePopup();
     profileTitle.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
 
